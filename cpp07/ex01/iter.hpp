c91f09308d9a8c0ae6449e46bf6e60a83f6c4eba
almat101 <sandruozzo@gmail.com>
2024-04-26 18:23:54 +0200

    add cpp07/ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,6 +3,34 @@
 
 #include <iostream>
 
+template<typename T>
+void print(T & element)
+{
+	std::cout << element << std::endl;
+}
+
+template<typename T>
+void iter(T *arr, size_t len, void(*func)(T&))
+{
+	for (size_t i = 0; i < len; i++)
+		func(arr[i]);
+}
+
+template<typename T>
+void increment(T & i)
+{
+	++i;
+}
+
+void upperCase(char& i)
+{
+	i = toupper(i);
+}
+
+void appendString(std::string& s)
+{
+	s += " world!";
+}
 
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,6 +3,38 @@
 
 int main(void)
 {
-	
+	size_t len = 6;
+	int arr[] = {42,9,0,21,13,39};
+	iter(arr , len , print<int>);
+
+	float arr3[] = {42.3f,9.4f,0.3f,21.1f,13.2f,39.04f};
+	iter(arr3 , len , print<float>);
+
+	char arr1[] = {'c','i','a','o','n','e'};
+	iter(arr1, len, print<char>);
+
+	std::string arr2[] = {"Hello","lol","lmao","HELL","kek","kkk"};
+	iter(arr2, len, print<std::string>);
+
+	std::cout << std::endl;
+	std::cout << "Other operation on arrays: " << std::endl;
+	iter(arr , len , increment);
+	for (size_t i = 0; i < len; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	iter(arr1, len, upperCase);
+	for (size_t i = 0; i < len; i++)
+		std::cout << arr1[i] << " ";
+	std::cout << std::endl;
+
+	iter(arr2, len, appendString);
+	for (size_t i = 0; i < len; i++)
+		std::cout << arr2[i] << " ";
+	std::cout << std::endl;
+
+
+
+
 	return 0;
 }
