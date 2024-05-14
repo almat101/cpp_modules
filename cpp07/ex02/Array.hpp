05ce2878a9793258eddc1db43850f49fbdd90c17
almat101 <sandruozzo@gmail.com>
2024-05-14 12:23:49 +0200

    complete cpp07/ex02
    add copy and assignement and the initialization to default value with unsigned int constructor

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -5,27 +5,32 @@
 
 template <typename T>
 class Array {
-    private:
-        T* data;  // Pointer to the dynamically allocated array
-        unsigned int size;  // Size of the array
+	private:
+		T* data;  // Pointer to the dynamically allocated array
+		unsigned int size;  // Size of the array
 
-    public:
-        // Construction with no parameter: Creates an empty array.
-        Array();
-        // Constructor (allocate memory)
-        // Construction with an unsigned int n as a parameter: Creates an array of n elements initialized by default.
-        Array(unsigned int capacity);
+	public:
+		// Constructor with no parameter: Creates an empty array.
+		Array();
+		// Constructor (allocate memory) with an unsigned int n as a parameter: Creates an array of n elements initialized by default.
+		Array(unsigned int capacity);
 
-        // Destructor (deallocate memory)
-        ~Array();
+		//copy constructor
+		Array(const Array &src);
 
-        // Access element at index (bounds checking)
-        T& operator[](int index);
+		//assignment operator
+		Array& operator=(const Array &rhs);
 
-        // Get the size of the array
-        int getSize() const { return size; }
+		// Destructor (deallocate memory)
+		~Array();
+		// Access element at index (bounds checking)
+		T& operator[](int index);
 
-        class array_out_of_range : public std::exception
+		// Get the size of the array
+		int getSize() const;
+
+		// Exception class
+		class array_out_of_range : public std::exception
 		{
 			// this function has to be virtual because override the method what() of the base class std::exception that is already virtual
 			// this is the declaration of what() in std::exception in c++98
@@ -33,42 +38,74 @@ class Array {
 		};
 };
 
+template <typename T>
+int Array<T>::getSize() const
+{
+	return size;
+}
 
 template <typename T>
 Array<T>::Array()
 {
-    data = NULL;
-    size = 0;
+	data = NULL;
+	size = 0;
 }
 
 template <typename T>
 Array<T>::Array(unsigned int capacity)
 {
-    data = new T[capacity];
-    size = capacity;   
+	data = new T[capacity](); // () to initialize to default value ( int to 0, char to '\0', double to 0.0, class to default constructor)
+	size = capacity;
 }
 
 template <typename T>
 Array<T>::~Array()
 {
-    if (data != NULL)
-        delete[] data;
+	if (data != NULL)
+		delete[] data;
 }
 
 template <typename T>
 T& Array<T>::operator[]( int index)
 {
-    if (index < 0 || index >= static_cast<int>(size))
-    {
-        throw Array<T>::array_out_of_range();
-    }
-    return data[index];
+	if (index < 0 || index >= static_cast<int>(size))
+	{
+		throw Array<T>::array_out_of_range();
+	}
+	return data[index];
 }
 
 template <typename T>
 const char* Array<T>::array_out_of_range::what() const throw()
 {
 	return "index is out of bound";
+}
+
+template <typename T>
+Array<T>::Array(const Array &src)
+{
+	size = src.size;
+	data = new T[size];
+	for (unsigned int i = 0; i < size; i++)
+	{
+		data[i] = src.data[i];
+	}
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array &rhs)
+{
+	if (this == &rhs)
+		return *this;
+	if (data != NULL)
+		delete[] data;
+	size = rhs.size;
+	data = new T[size];
+	for (unsigned int i = 0; i < size; i++)
+	{
+		data[i] = rhs.data[i];
+	}
+	return *this;
 }
 
 
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,41 +1,117 @@
 #include "Array.hpp"
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
 
 int main(void)
 {
 
-    Array<unsigned int> intArray(10);
+	// static int array[5]; // try to remove static and use valgrind to see the difference
 
-    
-    try
-    {
-        intArray[1] = 42;
-        unsigned int value = intArray[1];
-        std::cout << "Value at index 1: " << value << std::endl;
+	// for(int i = 0; i < 5; i++)
+	// {
+	// 	std::cout << array[i] << std::endl;
+	// }
 
-    } catch (const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
 
-    try {
-        unsigned int value = intArray[10];
-        std::cout << "Value at index 10: " << value << std::endl;
-    }
-    catch(const std::exception& e)
+	Array<unsigned int> intArray(5);
+	try
+	{
+		intArray[1] = 42;
+		for (int i = 0; i < intArray.getSize(); i++) {
+			std::cout << intArray[i] << " ";
+		}
+		std::cout << std::endl;
+
+	} catch (const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 
-   
-    Array<double> doubleArray(5);
+	try {
+		unsigned int value = intArray[10];
+		std::cout << "Value at index 10: " << value << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-    doubleArray[0] = 3.14;
-    doubleArray[2] = 2.7;
 
-    for (int i = 0; i < doubleArray.getSize(); i++) {
-        std::cout << doubleArray[i] << " ";
-    }
-    std::cout << std::endl;
+	Array<double> doubleArray(5);
 
-    return 0;
+	doubleArray[0] = 3.14;
+	doubleArray[2] = 2.7;
+	doubleArray[4] = 42.42;
+
+	for (int i = 0; i < doubleArray.getSize(); i++) {
+		std::cout << doubleArray[i] << " ";
+	}
+	std::cout << std::endl;
+
+	Array<char> charArray(3);
+
+	// charArray[0] = 'A';
+	charArray[1] = 'B'; //now works other char are initialized to 0
+	// charArray[2] = 'C';
+	for (int i = 0; i < charArray.getSize(); i++) {
+		std::cout << charArray[i] << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
+
+
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+// 	Array<int> numbers(MAX_VAL);
+// 	int* mirror = new int[MAX_VAL];
+// 	srand(time(NULL));
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		const int value = rand();
+// 		numbers[i] = value;
+// 		mirror[i] = value;
+// 	}
+// 	//SCOPE
+// 	{
+// 		Array<int> tmp = numbers;
+// 		Array<int> test(tmp);
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		if (mirror[i] != numbers[i])
+// 		{
+// 			std::cerr << "didn't save the same value!!" << std::endl;
+// 			return 1;
+// 		}
+// 	}
+// 	try
+// 	{
+// 		numbers[-2] = 0;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+// 	try
+// 	{
+// 		numbers[MAX_VAL] = 0;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 	}
+
+// 	for (int i = 0; i < MAX_VAL; i++)
+// 	{
+// 		numbers[i] = rand();
+// 	}
+// 	delete [] mirror;//
+// 	return 0;
+// }
+
